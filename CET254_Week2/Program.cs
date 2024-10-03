6c073cd061ea0b1d28a239da35c940bdde3510e0
bi23zv <bi23zv@FOT-CELLC25>
2024-10-03 10:14:27 +0100

    Add project files.

---
+++ CET254_Week2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CET254_Week2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
